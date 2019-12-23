8632ecef51681044806b7a49e4d567b1a9f85db8
Mitch Startzel <mstartzel16@gmail.com>
2019-12-22 23:54:44 -0500

    basic functionality

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var app = new Vue({ 
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
+var app = new Vue({
+  el: '#app',
+  data: {
+    gold: 1,
+    peonCount: 0
+  },
+  methods: {
+    clickGold: function () {
+      this.gold += 1
+    },
+    clickPeon: function () {
+      this.peonCount += 1
     }
-});
+  }
+})
 
-var app2 = new Vue({ 
-    el: '#app2',
-    data: {
-        message: 'Name Jeff!'
-    }
-});
+var gold = 0;
