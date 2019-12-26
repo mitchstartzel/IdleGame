c20735869bebd3164899e7984adb5e36d0f98e88
Mitch Startzel <mstartzel16@gmail.com>
2019-12-26 00:07:13 -0500

    first real version - alpha 1.0

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-Vue.component('prog-bar', {props: ['name'], template: '<div class="myProgress"><div class="myBar" :id="name"></div></div>'})
+Vue.component('prog-bar', {props: {name: Number}, template: '<img src="https://i.imgur.com/acx5LyQ.png" class="test_peon" :id=name>'})
 
 var app = new Vue({
   el: '#app',
   data: {
-    gold: 1,
+    gold: 0,
     peonCount: 0,
+    peonCost: 10,
     peons: []
   },
   methods: {
@@ -12,16 +13,51 @@ var app = new Vue({
 		this.gold += 1
     },
     clickPeon: function () {
-		if (this.gold >= 10){
-			this.gold -= 10
+		if (this.gold >= this.peonCost){
+			this.gold -= this.peonCost
+            this.peonCost = parseInt(10*(1.25**(this.peonCount+1)))
 			this.peonCount += 1
-            this.peons.push({text: "Peon" + this.peonCount, progress: 0})
+            this.peons.push({name: this.peonCount, mLeft: -95, mTop: 500, returning: false})
 		}
     }
   }
 })
 
+function myMove() {
+   var id = setInterval(frame, 40)
+    function frame() {
+        for (i = 0; i < app.peonCount; i++) {
+            var peon = app.peons[i]
+            var elem = document.getElementById(peon.name);
+            if (!peon.returning){
+                if (peon.mTop <= 125) {
+                    elem.style.zIndex = 2;
+                    peon.returning = true;
+                } else if (peon.mTop > 100) {
+                    peon.mTop -= 3;
+                    peon.mLeft -= 1;
+                    elem.style.marginTop = peon.mTop + 'px';
+                    elem.style.marginLeft = peon.mLeft + 'px';
+                }
+            } else {
+                peon.mTop += 3;
+                peon.mLeft += 1;
+                if (peon.mTop >= 500) {
+                    app.gold += 10;
+                    peon.returning = false
+                    elem.style.zIndex = 3;
+                } else {
+                    elem.style.marginTop = peon.mTop + 'px';
+                    elem.style.marginLeft = peon.mLeft + 'px';
+                }
+            }
+            
+        }
+    
+    }
+}
 
+myMove()
 
 function moveBars() {
     
@@ -39,10 +75,7 @@ function moveBars() {
     
     }
 }
-
-
-
-moveBars()
+//moveBars()
 
 
 
